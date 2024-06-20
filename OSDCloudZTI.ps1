631abe3ce4082dbce6b8a1a32a47e22838573f33
atenpierick <154617355+atenpierick@users.noreply.github.com>
2024-06-20 11:43:31 +0200

    Update OSDCloudZTI.ps1

--- OSDCloudZTI.ps1
+++ OSDCloudZTI.ps1
@@ -10,7 +10,7 @@ Import-Module OSD -Force
 
 #Start OSDCloud ZTI
 Write-Host  -ForegroundColor Cyan "Start OSDCloud ZTI Mode"
-#Start-OSDCloud -OSName 'Windows 10 22H2 x64' -OSLanguage nl-nl -OSEdition Pro -ZTI
+#Start-OSDCloud -OSName "Windows 10 22H2 x64" -OSLanguage nl-nl -OSEdition Pro -ZTI
 Start-OSDCloud
 
 #Restart from WinPE
