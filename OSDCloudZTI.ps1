58bf5df470011dc45635844bd3340806e6af396a
atenpierick <154617355+atenpierick@users.noreply.github.com>
2024-06-20 12:03:11 +0200

    Update OSDCloudZTI.ps1

--- OSDCloudZTI.ps1
+++ OSDCloudZTI.ps1
@@ -3,7 +3,7 @@ Start-Sleep -Seconds 5
 
 #Updating the OSD PowerShell Module
 Write-Host  -ForegroundColor Cyan "Updating the OSD PowerShell Module"
-Update-Module OSD -Force
+Update-Module OSD -Force -SkipPublisherCheck
 
 Write-Host  -ForegroundColor Cyan "Importing the OSD PowerShell Module"
 Import-Module OSD -Force
